db1d3bec2fb2172afb7e096c4f16955668b66e68
Uppada Ananda Rao <115538489+a2zrao@users.noreply.github.com>
2023-09-24 17:24:10 +0530

    Add files via upload

---
+++ Project1/a2zpwdgen.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+
+echo " ██▓███   ▄▄▄        ██████   ██████  █     █░ ▒█████   ██▀███  ▓█████▄      ▄████ ▓█████  ███▄    █ ▓█████  ██▀███   ▄▄▄     ▄▄▄█████▓ ▒█████   ██▀███  ";
+echo "▓██░  ██▒▒████▄    ▒██    ▒ ▒██    ▒ ▓█░ █ ░█░▒██▒  ██▒▓██ ▒ ██▒▒██▀ ██▌    ██▒ ▀█▒▓█   ▀  ██ ▀█   █ ▓█   ▀ ▓██ ▒ ██▒▒████▄   ▓  ██▒ ▓▒▒██▒  ██▒▓██ ▒ ██▒";
+echo "▓██░ ██▓▒▒██  ▀█▄  ░ ▓██▄   ░ ▓██▄   ▒█░ █ ░█ ▒██░  ██▒▓██ ░▄█ ▒░██   █▌   ▒██░▄▄▄░▒███   ▓██  ▀█ ██▒▒███   ▓██ ░▄█ ▒▒██  ▀█▄ ▒ ▓██░ ▒░▒██░  ██▒▓██ ░▄█ ▒";
+echo "▒██▄█▓▒ ▒░██▄▄▄▄██   ▒   ██▒  ▒   ██▒░█░ █ ░█ ▒██   ██░▒██▀▀█▄  ░▓█▄   ▌   ░▓█  ██▓▒▓█  ▄ ▓██▒  ▐▌██▒▒▓█  ▄ ▒██▀▀█▄  ░██▄▄▄▄██░ ▓██▓ ░ ▒██   ██░▒██▀▀█▄  ";
+echo "▒██▒ ░  ░ ▓█   ▓██▒▒██████▒▒▒██████▒▒░░██▒██▓ ░ ████▓▒░░██▓ ▒██▒░▒████▓    ░▒▓███▀▒░▒████▒▒██░   ▓██░░▒████▒░██▓ ▒██▒ ▓█   ▓██▒ ▒██▒ ░ ░ ████▓▒░░██▓ ▒██▒";
+echo "▒▓▒░ ░  ░ ▒▒   ▓▒█░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░░ ▓░▒ ▒  ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░ ▒▒▓  ▒     ░▒   ▒ ░░ ▒░ ░░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒▓ ░▒▓░ ▒▒   ▓▒█░ ▒ ░░   ░ ▒░▒░▒░ ░ ▒▓ ░▒▓░";
+echo "░▒ ░       ▒   ▒▒ ░░ ░▒  ░ ░░ ░▒  ░ ░  ▒ ░ ░    ░ ▒ ▒░   ░▒ ░ ▒░ ░ ▒  ▒      ░   ░  ░ ░  ░░ ░░   ░ ▒░ ░ ░  ░  ░▒ ░ ▒░  ▒   ▒▒ ░   ░      ░ ▒ ▒░   ░▒ ░ ▒░";
+echo "░░         ░   ▒   ░  ░  ░  ░  ░  ░    ░   ░  ░ ░ ░ ▒    ░░   ░  ░ ░  ░    ░ ░   ░    ░      ░   ░ ░    ░     ░░   ░   ░   ▒    ░      ░ ░ ░ ▒    ░░   ░ ";
+echo "               ░  ░      ░        ░      ░        ░ ░     ░        ░             ░    ░  ░         ░    ░  ░   ░           ░  ░            ░ ░     ░     ";
+echo "                                                                 ░                                                                                       ";
+echo " ▄▄▄▄ ▓██   ██▓    ▄▄▄       ███▄    █  ▄▄▄       ███▄    █ ▓█████▄  ▄▄▄          ██▀███   ▄▄▄       ▒█████                                              ";
+echo "▓█████▄▒██  ██▒   ▒████▄     ██ ▀█   █ ▒████▄     ██ ▀█   █ ▒██▀ ██▌▒████▄       ▓██ ▒ ██▒▒████▄    ▒██▒  ██▒                                            ";
+echo "▒██▒ ▄██▒██ ██░   ▒██  ▀█▄  ▓██  ▀█ ██▒▒██  ▀█▄  ▓██  ▀█ ██▒░██   █▌▒██  ▀█▄     ▓██ ░▄█ ▒▒██  ▀█▄  ▒██░  ██▒                                            ";
+echo "▒██░█▀  ░ ▐██▓░   ░██▄▄▄▄██ ▓██▒  ▐▌██▒░██▄▄▄▄██ ▓██▒  ▐▌██▒░▓█▄   ▌░██▄▄▄▄██    ▒██▀▀█▄  ░██▄▄▄▄██ ▒██   ██░                                            ";
+echo "░▓█  ▀█▓░ ██▒▓░    ▓█   ▓██▒▒██░   ▓██░ ▓█   ▓██▒▒██░   ▓██░░▒████▓  ▓█   ▓██▒   ░██▓ ▒██▒ ▓█   ▓██▒░ ████▓▒░                                            ";
+echo "░▒▓███▀▒ ██▒▒▒     ▒▒   ▓▒█░░ ▒░   ▒ ▒  ▒▒   ▓▒█░░ ▒░   ▒ ▒  ▒▒▓  ▒  ▒▒   ▓▒█░   ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ▒░▒░▒░                                             ";
+echo "▒░▒   ░▓██ ░▒░      ▒   ▒▒ ░░ ░░   ░ ▒░  ▒   ▒▒ ░░ ░░   ░ ▒░ ░ ▒  ▒   ▒   ▒▒ ░     ░▒ ░ ▒░  ▒   ▒▒ ░  ░ ▒ ▒░                                             ";
+echo " ░    ░▒ ▒ ░░       ░   ▒      ░   ░ ░   ░   ▒      ░   ░ ░  ░ ░  ░   ░   ▒        ░░   ░   ░   ▒   ░ ░ ░ ▒                                              ";
+echo " ░     ░ ░              ░  ░         ░       ░  ░         ░    ░          ░  ░      ░           ░  ░    ░ ░                                              ";
+echo "      ░░ ░                                                   ░                                                                                           ";
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                                                         --Password Generator-- "
+echo "                                                     **Made by Uppada Ananda Rao** "
+echo "                                                   /*  https://uppada4u.web.app/  */  "
+
+
+
+echo "This is a passowrd generator based on Users desired Length and Algorithm"
+
+options="-base64 -hex"
+
+read -p "Enter the length of the password : " pwd
+echo "Select the algorith you want to use for generation : "
+select opt in $options; do
+	if [ "$opt" = "-base64" ]; then
+		for p in $(seq 1); do
+			openssl rand -base64 48 | cut -c1-$pwd
+			echo " ->Base64 is the algorithm which will provide the most secure password with combination of capital, small letters and numeric with special character "
+			echo "Selecting a Better algorithm for passowrd generation is good practice, beleaving which will help reduse the risk of Brute Force Attacks"
+			echo "Thank You for using this tool"
+		done
+		break
+	elif [ "$opt" = "-hex" ]; then
+		for i in $(seq 1); do
+			openssl rand -hex 48 | cut -c1-$pwd
+			echo " ->Hex is the algorithm which will provide the secure password with combination of small letters and numeric character"
+			echo "Selecting a Better algorithm for passowrd generation is good practice, beleaving which will help reduse the risk of Brute Force Attacks"
+			echo "Thank You for using this tool"
+		done
+		break
+	fi
+done
